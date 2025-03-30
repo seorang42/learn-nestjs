75c09c25ea408d16d9e99d161b7d5365e3642565
seorang42 <seorang42@naver.com>
2025-03-30 20:28:49 +0900

    5-5 Posts 모듈 생성하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-@Controller('post')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('post')
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 100000,
-      commentCount: 999999,
-    };
-  }
 }
