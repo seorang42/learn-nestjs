d11158bc9faec976844e49c3155c68ba3e7e66ce
seorang42 <seorang42@naver.com>
2025-03-30 19:51:46 +0900

    5-3 포스트 JSON 반환하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHome() {
-    return 'Home Page';
-  }
-
-  @Get('post')
-  getPost() {
-    return 'Post Page';
-  }
-
-  @Get('user')
-  getUser() {
-    return 'User Page';
+  getPost(): Post {
+    return {
+      author: 'newjeans_official',
+      title: '뉴진스 민지',
+      content: '메이크업 고치고 있는 민지',
+      likeCount: 100000,
+      commentCount: 999999,
+    };
   }
 }
