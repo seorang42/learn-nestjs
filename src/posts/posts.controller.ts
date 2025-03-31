8c000b2514e1b365ef9f60c59477079c7c3e040f
seorang42 <seorang42@naver.com>
2025-03-31 19:48:10 +0900

    6-6 Put Post 엔드포인트 생성하기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -5,6 +5,7 @@ import {
   NotFoundException,
   Param,
   Post,
+  Put,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
@@ -92,6 +93,35 @@ export class PostsController {
 
   // 4) PUT /posts/:id
   //    id에 해당되는 POST를 변경한다
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
+
+    return post;
+  }
 
   // 5) DELETE /posts/:id
   //    id에 해당되는 POST를 삭제한다
