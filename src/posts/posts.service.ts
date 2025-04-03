c9163422f9b45dee1872ff2dfb31456c37b4beea
seorang42 <seorang42@naver.com>
2025-04-04 01:11:18 +0900

    10-7 Save 함수로 업데이트 하기

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -81,8 +81,21 @@ export class PostsService {
     return newPost;
   }
 
-  updatePost(postId: number, author: string, title: string, content: string) {
-    const post = posts.find((post) => post.id === postId);
+  async updatePost(
+    postId: number,
+    author: string,
+    title: string,
+    content: string,
+  ) {
+    // save의 기능
+    // 1) 데이터가 존재하지 않는다면 (id 기준으로) 새로 생성한다.
+    // 2) 데이터가 존재한다면 (같은 id의 값이 존재한다면) 존재하던 값을 업데이트한다.
+
+    const post = await this.postsRepository.findOne({
+      where: {
+        id: postId,
+      },
+    });
 
     if (!post) {
       throw new NotFoundException();
@@ -100,9 +113,9 @@ export class PostsService {
       post.content = content;
     }
 
-    posts = posts.map((prevPost) => (prevPost.id === postId ? post : prevPost));
+    const newPost = await this.postsRepository.save(post);
 
-    return post;
+    return newPost;
   }
 
   deletePost(postId: number) {
