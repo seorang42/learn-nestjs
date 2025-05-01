16a977f409b19a84037eec744407316f4553ec26
seorang42 <seorang42@naver.com>
2025-05-01 22:21:48 +0900

    12-3 Column 옵션 탐구

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -5,10 +5,16 @@ export class UsersModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  // 1) 길이가 20을 넘지 않을 것
+  // 2) 유일무이한 값이 될 것
+  @Column({
+    length: 20,
+    unique: true,
+  })
   nickname: string;
 
-  @Column()
+  // 1) 유일무이한 값이 될 것
+  @Column({ unique: true })
   email: string;
 
   @Column()
